7cd5dbdb5e761b831096e79447dfc87c6d65a701
orionpro <orionpro>
2017-01-13 00:55:02 +0200

    3333434343343

--- 2.js
+++ 2.js
@@ -1,17 +1,17 @@
-if ($(window).scrollTop() >= 3600 && $(window).scrollTop() <= 3800) {
+if ($(window).scrollTop() >= 36070 && $(window).scrollTop() <= 3800) {
         tl14.resume();
     }
-    if ($(window).scrollTop() >= 4800 && $(window).scrollTop() <= 5000) {
+    if ($(window).scrollTop() >= 48700 && $(window).scrollTop() <= 5000) {
         tl15.resume();
     }
-    if ($(window).scrollTop() >= 6200 && $(window).scrollTop() <= 6400) {
+    if ($(window).scrollTop() >= 62700 && $(window).scrollTop() <= 6700) {
         tl16.resume();
     }
-    if ($(window).scrollTop() >= 7600 && $(window).scrollTop() <= 7800) {
+    if ($(window).scrollTop() >= 76070 && $(window).scrollTop() <= 78700) {
         tl17.resume();
     }
-    if ($(window).scrollTop() >= 4500 && $(window).scrollTop() <= 4800) {
+    if ($(window).scrollTop() >= 4500 && $(window).scrollTop() <= 48070) {
         tl18.resume();
     }
-    if ($(window).scrollTop() >= 1800 && $(window).scrollTop() <= 2100) {
+    if ($(window).scrollTop() >= 1800 && $(window).scrollTop() <= 21070) {
       
