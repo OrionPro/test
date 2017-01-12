46c1267df576c09950044ffd9d4250e493693642
orionpro <orionpro>
2017-01-13 00:50:31 +0200

    123234

--- 2.js
+++ 2.js
@@ -1,5 +1,6 @@
-343453455
-434535345345345
-222222222222sfsdfsfsdfs
-333333333333
-1313131131312
+if ($(window).scrollTop() >= 3800 && $(window).scrollTop() <= 3900) {
+    tl9.resume();
+}
+if ($(window).scrollTop() >= 5700 && $(window).scrollTop() <= 5900) {
+    tl10.resume();
+}
