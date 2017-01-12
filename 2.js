1edf63103755dba0c468fcf725077b4a9af3b682
OrionPro <orion79@rambler.ru>
2017-01-13 01:07:19 +0200

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,12 +1,17 @@
-var tl = new TimelineMax(); // скалы и дома вверху
-var tl2 = new TimelineMax(); //  ковбой
-var tl3 = new TimelineMax(); // what_else_do_line
-var tl4 = new TimelineMax(); // Наше порфтолио
-var tl5 = new TimelineMax(); // our_achievements
-var tl6 = new TimelineMax({ repeat: -1, yoyo: true }); //анимация стрелок
-var tl7 = new TimelineMax(); // our_achievements_item 
-var tl8 = new TimelineMax(); // take_part_img 
-var tl9 = new TimelineMax(); // take_part_img_bot_decor 
-var tl10 = new TimelineMax(); // book_now_city 
-  
+    tl5.pause(); // our_achievements
+    tl7.pause(); // our_achievements_item 
+    tl8.pause(); // take_part_img 
+    tl9.pause(); // take_part_img_bot_decor
+    tl10.pause(); // book_now_city
+    tl12.pause(); // monitor
+    tl13.pause(); // why_seo item
+    tl14.pause(); // from_what_is_seo item
+    tl15.pause(); // stages_of_advancement item
+    tl16.pause(); // cost_optimization item
+    tl17.pause(); // our_work_item
+    tl18.pause(); // our_work_itemfrom_what_is_seo_bot_decor
+    tl21.pause(); // from_idea_to_results_item
+    tl22.pause(); // website_promotion_decor
+    tl23.pause(); // from_idea_to_results_decor
+    tl25.pause(); // does_the_sale_item_img_circle
       
