43785357760c6cbf58a2bebe07c43d6efe1a12f3
OrionPro <orion79@rambler.ru>
2017-01-13 01:05:56 +0200

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,15 +1,12 @@
-    2
-    if ($(window).scrollTop() >= 4800 && $(window).scrollTop() <= 5000) {
-        tl15.resume();
-    
-    if ($(window).scrollTop() >= 6200 && $(window).scrollTop() <= 6400) {
-        tl16.resume();
-    }
-    if ($(window).scrollTop() >= 7600 && $(window).scrollTop() <= 7800) {
-        tl17.resume();
-    }
-    if ($(window).scrollTop() >= 4500 && $(window).scrollTop() <= 4800) {
-        tl18.resume();
-    }
+var tl = new TimelineMax(); // скалы и дома вверху
+var tl2 = new TimelineMax(); //  ковбой
+var tl3 = new TimelineMax(); // what_else_do_line
+var tl4 = new TimelineMax(); // Наше порфтолио
+var tl5 = new TimelineMax(); // our_achievements
+var tl6 = new TimelineMax({ repeat: -1, yoyo: true }); //анимация стрелок
+var tl7 = new TimelineMax(); // our_achievements_item 
+var tl8 = new TimelineMax(); // take_part_img 
+var tl9 = new TimelineMax(); // take_part_img_bot_decor 
+var tl10 = new TimelineMax(); // book_now_city 
   
       
