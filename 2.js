57f592c0aacd794eb2a2e9d11be568dd4f61bb36
OrionPro <orion79@rambler.ru>
2017-01-13 00:53:15 +0200

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,4 +1,7 @@
-if ($(window).scrollTop() >= 4800 && $(window).scrollTop() <= 5000) {
+if ($(window).scrollTop() >= 3600 && $(window).scrollTop() <= 3800) {
+        tl14.resume();
+    }
+    if ($(window).scrollTop() >= 4800 && $(window).scrollTop() <= 5000) {
         tl15.resume();
     }
     if ($(window).scrollTop() >= 6200 && $(window).scrollTop() <= 6400) {
@@ -7,3 +10,8 @@ if ($(window).scrollTop() >= 4800 && $(window).scrollTop() <= 5000) {
     if ($(window).scrollTop() >= 7600 && $(window).scrollTop() <= 7800) {
         tl17.resume();
     }
+    if ($(window).scrollTop() >= 4500 && $(window).scrollTop() <= 4800) {
+        tl18.resume();
+    }
+    if ($(window).scrollTop() >= 1800 && $(window).scrollTop() <= 2100) {
+      
