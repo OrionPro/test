73956a6e2a219d5893c147510f0f45b11bd04528
orionpro <orionpro>
2017-01-13 01:13:07 +0200

    35353

--- 2.js
+++ 2.js
@@ -1,4 +1,31 @@
-  // цели
+// what_else_do_line
+tl3.from("#path4159", 0.5, { drawSVG: "0%" }, 0.4);
+tl3.from("#path4157", 5.5, { drawSVG: "0%" }, 0.4);
+tl3.from("#path4164", 2, { fill: "#031420", drawSVG: "0%" }, 5);
+tl3.from("#g4409", 0.8, { opacity: 0, transformOrigin: "50% 50%", scale: 0 }, 0.9);
+tl3.from("#g6391", 0.8, { opacity: 0, transformOrigin: "50% 50%", scale: 0 }, 1.1);
+tl3.from("#g6814", 0.8, { opacity: 0, transformOrigin: "50% 50%", scale: 0 }, 1.3);
+tl3.from("#g4995", 0.8, { opacity: 0, transformOrigin: "50% 50%", scale: 0 }, 1.5);
+tl3.from((".what_else_do_item_p"), 0.7, { opacity: 0, transformOrigin: "50% 50%", scale: 0 }, 1);
+tl3.from("#g42911111", 0.8, { opacity: 0, transformOrigin: "50% 50%", scale: 0 }, 1.7);
+
+// Наше портфолио
+tl4.from("#g4388", 0.8, { opacity: 0, x: 100, ease: Power2.easeInOut, }, 0.3);
+tl4.from("#g4474", 0.8, { opacity: 0, y: -100, ease: Power2.easeInOut, }, 0.4);
+tl4.from("#g4420", 0.8, { opacity: 0, y: -100, ease: Power2.easeInOut, }, 0.5);
+tl4.from("#g4504", 0.8, { opacity: 0, x: -100, ease: Power2.easeInOut, }, 0.6);
+tl4.from("#rect4286", 0.8, { opacity: 0, x: -100, ease: Power2.easeInOut, }, 0.7);
+
+
+// our_achievements
+tl5.from("#path4202", 1.5, { drawSVG: "0%" }, 0.2);
+tl5.from("#g4185235425235", 0.2, { opacity: 0, y: -50 }, 1.8);
+tl5.from("#path5234234", 0.5, { opacity: 0, y: -50 }, 1.4);
+tl5.from("#path41782342423", 3.5, { drawSVG: "0%" }, 1.9);
+tl5.from("#path723523253", 1.5, { drawSVG: "0%" }, 4.9);
+tl5.from("#path_go", 1.5, { drawSVG: "100% 100%", ease: Power2.easeInOut }, 2.1);
+tl5.from("#text4221", 0.5, { opacity: 0, x: -50, ease: Power2.easeInOut }, 2.5);
+// цели
     TweenMax.from("#g4378", 1.5, { rotation: 360, transformOrigin: "50% 50%", delay: 0.5, ease: Power1.easeInOut });
     TweenMax.from("#g4950", 1.5, { rotation: 360, transformOrigin: "50% 50%", delay: 0.5, ease: Power1.easeInOut });
     TweenMax.from("#g4219", 1.5, { rotation: 360, transformOrigin: "50% 50%", delay: 0.5, ease: Power1.easeInOut });
